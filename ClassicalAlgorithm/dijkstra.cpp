69de41c92b9649aee8ecd7f89ffcc33fa5aad496
Leon <1342723111@qq.com>
2019-07-02 15:03:58 +0800

    feat(dijkstra.cpp)

--- ClassicalAlgorithm/dijkstra.cpp
+++ ClassicalAlgorithm/dijkstra.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #define INF 1 << 30
 #define MAXN 505
 using namespace std;
@@ -8,32 +8,43 @@ int dist[MAXN];
 bool vis[MAXN];
 int N, M;
 
-void init(){
-    for(int i = 0; i < MAXN; i++){
-        for(int j = 0; j < MAXN; j++){
-            if(i == j) edges[i][j] = 0;
-            else edges[i][j] = INF;
+void init()
+{
+    for (int i = 0; i < MAXN; i++)
+    {
+        for (int j = 0; j < MAXN; j++)
+        {
+            if (i == j)
+                edges[i][j] = 0;
+            else
+                edges[i][j] = INF;
         }
     }
-    for(int i = 0; i < MAXN; i++){
+    for (int i = 0; i < MAXN; i++)
+    {
         dist[i] = INF;
         vis[i] = false;
     }
-    cin>>N>>M;
-    for(int i = 1; i <= M; i++){
+    cin >> N >> M;
+    for (int i = 1; i <= M; i++)
+    {
         int s, e, cost;
-        cin>>s>>e>>cost;
+        cin >> s >> e >> cost;
         edges[s][e] = cost;
     }
 }
 
-int dijkstra(int s, int e){
+int dijkstra(int s, int e)
+{
     dist[s] = 0;
-    for(int i = 1; i <= N; i++){
+    for (int i = 1; i <= N; i++)
+    {
         int min = INF;
         int x = -1;
-        for(int j = 1; j <= N; j++){
-            if(!vis[j] && dist[j] < min){
+        for (int j = 1; j <= N; j++)
+        {
+            if (!vis[j] && dist[j] < min)
+            {
                 min = dist[j];
                 x = j;
             }
@@ -41,8 +52,10 @@ int dijkstra(int s, int e){
 
         vis[x] = true;
 
-        for(int j = 1; j <= N; j++){
-            if(!vis[j] && dist[j] > dist[x] + edges[x][j]){
+        for (int j = 1; j <= N; j++)
+        {
+            if (!vis[j] && dist[j] > dist[x] + edges[x][j])
+            {
                 dist[j] = dist[x] + edges[x][j];
             }
         }
@@ -50,9 +63,9 @@ int dijkstra(int s, int e){
     return dist[e];
 }
 
-
-int main(){
+int main()
+{
     init();
-    cout<<dijkstra(1, 4)<<endl;
+    cout << dijkstra(1, 4) << endl;
     return 0;
 }
